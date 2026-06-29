db7f7a7d7d948135b1cf99b35b7db4a369d3f4a2
Barrio <andrea.barrio03@gmail.com>
2026-06-29 22:53:56 +0200

    Update ANGSD_LmR.sh

--- ANGSD_LmR.sh
+++ ANGSD_LmR.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
 
-pth_home=/PATH/DIRECTORY
-pth_angsd=/PATH/angsd
-pth_i=$pth_home
-pth_o=$pth_home/ANGSD_Results
-i_name=LmR.bamlist.txt
-o_name=LmR_angsd
-ref=/PATH/bLanMin1.curated_primary.no_mt.scrubbed.fa
+ref=bLanMin1.curated_primary.no_mt.scrubbed.fa
 pth_anc=$ref
-bedfile=/PATH/repeats.Lm.negative_format.SUPER.bed
+bedfile=repeats.Lm.negative_format.SUPER.bed
 threads=8
 min_depth=6
 max_depth=101
 
-$pth_angsd/angsd \
+angsd \
      -GL 2 \
-     -out $pth_o/$o_name \
+     -out LmR_angsd \
      -nThreads $threads \
      -doSaf 1 \
      -anc $pth_anc \
@@ -31,4 +25,4 @@ $pth_angsd/angsd \
      -setMinDepth $min_depth \
      -setMaxDepth $max_depth \
      -doCounts 1 \
-     -bam /PATH/LmR.bamlist.txt
+     -bam LmR.bamlist.txt
