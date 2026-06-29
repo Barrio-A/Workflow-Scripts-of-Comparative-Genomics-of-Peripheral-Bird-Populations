949e41fc66d7c4a188538277322e91fe3fdc4cb8
Barrio <andrea.barrio03@gmail.com>
2026-06-29 22:30:28 +0200

    Update Whole-Chr._rapid.pi.Est_LmR.sh

--- Whole-Chr._rapid.pi.Est_LmR.sh
+++ Whole-Chr._rapid.pi.Est_LmR.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-awk '$2 != "NA" {sum += $2; n++} END {if(n>0) print sum/n}' AfS_diversity_mean_per_chromosome.txt > result_mean_genome_AfS.txt
+awk '$2 != "NA" {sum += $2; n++} END {if(n>0) print sum/n}' LmR_diversity_mean_per_chromosome.txt > result_mean_genome_LmR.txt
