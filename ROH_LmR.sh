79d2b956dcc99746ccb0bc4c2ff4452de1074869
Barrio <andrea.barrio03@gmail.com>
2026-06-29 20:53:02 +0200

    Update ROH_LmR.sh

--- ROH_LmR.sh
+++ ROH_LmR.sh
@@ -5,7 +5,7 @@ rohan \
     --auto autosomes \
     --size 1000000 \
     -t 24 \
-    --map Lm.mask.bed \
+    --map repeats.Lm.negative_format.SUPER.bed \
     -o LmR.rhomu2e5.1m.rohan \
     /PATH/bLanMin1.curated_primary.no_mt.scrubbed.fa \
     /PATH/LmR.bam
